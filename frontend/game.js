9f55d6989d67f3c85f490401689df07d415b1f4e
Davis Robertson <epicdavibrickfilms@gmail.com>
2015-07-24 20:40:18 -0500

    Add drawing rectangles

--- frontend/game.js
+++ frontend/game.js
@@ -25,10 +25,17 @@ function render(){
     drawPixel(0, 0, "black");
     drawPixel(0, 1, "red");
     drawPixel(1, 0, "green");
+    drawPixelRectange(1, 1, 2, 2, "blue");
     anim_frame(render);
 }
 
 function drawPixel(x, y, color){
     ctx.fillStyle=color;
     ctx.fillRect(x*pixel_width, game_height-(y+1)*pixel_width, pixel_width, pixel_width);
+}
+
+function drawPixelRectange(x1, y1, x2, y2, color){
+    var minx=Math.min(x1, x2), miny=Math.min(y1, y2), maxx=Math.max(x1, x2), maxy=Math.max(y1, y2);
+    ctx.fillStyle=color;
+    ctx.fillRect(minx*pixel_width, game_height-(miny+2)*pixel_width, (maxx-minx+1)*pixel_width, (maxy-miny+1)*pixel_width);
 }
