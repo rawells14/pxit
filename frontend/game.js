57461a048df4dbbfaed8b7f90d321e69060cfd19
Davis Robertson <epicdavibrickfilms@gmail.com>
2015-07-24 20:27:58 -0500

    Fixing Conflicts and Adding Pixels

--- frontend/game.js
+++ frontend/game.js
@@ -1,5 +1,4 @@
 
-
 var anim_frame = window.requestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     window.webkitRequestAnimationFrame;
@@ -7,15 +6,29 @@ var anim_frame = window.requestAnimationFrame ||
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
+var game_width = 800;
+var game_height = 600;
+var pixel_width = 10;
+
+var game_objects = {
+};
+
 
 window.onload = function(){
-    console.log("asdf");
-    anim_frame(render);
     $('#login-modal').modal('show');
+    anim_frame(render);
 };
 
 function render(){
     ctx.moveTo(0,0);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPixel(0, 0, "black");
+    drawPixel(0, 1, "red");
+    drawPixel(1, 0, "green");
     anim_frame(render);
+}
+
+function drawPixel(x, y, color){
+    ctx.fillStyle=color;
+    ctx.fillRect(x*pixel_width, game_height-(y+1)*pixel_width, pixel_width, pixel_width);
 }
