ca7bc38097004801d07f980754816a458e721406
Davis Robertson <epicdavibrickfilms@gmail.com>
2015-07-24 20:09:15 -0500

    Updating game

--- frontend/game.js
+++ frontend/game.js
@@ -1,4 +1,21 @@
 
-var canvas = document.getElementById("game-canvas");
-var contex = canvas.getContext("2d");
 
+var anim_frame = window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame;
+
+var canvas = document.getElementById("game-canvas");
+var ctx = canvas.getContext("2d");
+
+
+window.onload = function(){
+    console.log("asdf");
+    anim_frame(render);
+
+};
+
+function render(){
+    ctx.moveTo(0,0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    anim_frame(render);
+}
