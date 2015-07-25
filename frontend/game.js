ae4bf5eead347f73aadda3176c6258a986dcd980
Ryan Wells <rawells14@gmail.com>
2015-07-24 21:14:54 -0500

    Finished Modal

--- frontend/game.js
+++ frontend/game.js
@@ -1,4 +1,3 @@
-
 var anim_frame = window.requestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     window.webkitRequestAnimationFrame;
@@ -9,23 +8,23 @@ var ctx = canvas.getContext("2d");
 var game_width = 800;
 var game_height = 600;
 var pixel_width = 10;
-var pixelsx = game_width/pixel_width;
-var pixelsy = game_height/pixel_width;
+var pixelsx = game_width / pixel_width;
+var pixelsy = game_height / pixel_width;
 
-function Pixel(x, y, color){
-    this.x=x;
-    this.y=y;
-    this.draw= function () {
+function Pixel(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.draw = function () {
         drawPixel(x, y, color);
     }
 }
 
-function Rectangle(x1, y1, x2, y2, color){
-    this.x1=x1;
-    this.y1=y1;
-    this.x2=x2;
-    this.y2=y2;
-    this.color=color;
+function Rectangle(x1, y1, x2, y2, color) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.color = color;
     this.draw = function () {
         drawRectange(x1, y1, x2, y2, color);
     }
@@ -33,28 +32,31 @@ function Rectangle(x1, y1, x2, y2, color){
 
 
 var game_objects = {
-    players :[
+    players: [],
+    collide: [
+        new Rectangle(0, 0, pixelsx, pixelsy / 2, "green")
     ],
-    collide :[
-        new Rectangle(0, 0, pixelsx, pixelsy/2, "green")
-    ],
-    background :[
-    ]
+    background: []
 };
 
 
-window.onload = function(){
-    $('#login-modal').modal('show');
+window.onload = function () {
+    $('#login-modal').modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+
+
     anim_frame(render);
 
 };
 
-function draw(obj){
+function draw(obj) {
     obj.draw();
 }
 
-function render(time){
-    ctx.moveTo(0,0);
+function render(time) {
+    ctx.moveTo(0, 0);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     game_objects.players.map(draw);
     game_objects.collide.map(draw);
@@ -62,14 +64,14 @@ function render(time){
     anim_frame(render);
 }
 
-function drawPixel(x, y, color){
-    ctx.fillStyle=color;
-    ctx.fillRect(x*pixel_width, game_height-(y+1)*pixel_width, pixel_width, pixel_width);
+function drawPixel(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * pixel_width, game_height - (y + 1) * pixel_width, pixel_width, pixel_width);
 }
 
-function drawRectange(x1, y1, x2, y2, color){
-    var minx=Math.min(x1, x2), miny=Math.min(y1, y2), maxx=Math.max(x1, x2), maxy=Math.max(y1, y2);
-    ctx.fillStyle=color;
-    ctx.fillRect(minx*pixel_width, game_height-(maxy+1)*pixel_width, (maxx-minx+1)*pixel_width, (maxy-miny+1)*pixel_width);
+function drawRectange(x1, y1, x2, y2, color) {
+    var minx = Math.min(x1, x2), miny = Math.min(y1, y2), maxx = Math.max(x1, x2), maxy = Math.max(y1, y2);
+    ctx.fillStyle = color;
+    ctx.fillRect(minx * pixel_width, game_height - (maxy + 1) * pixel_width, (maxx - minx + 1) * pixel_width, (maxy - miny + 1) * pixel_width);
 }
 
